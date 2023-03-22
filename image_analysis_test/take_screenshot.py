4c185b59311ea1146abb35bedaa56de0581155d5
Alex <alexander.reiprich@hs-furtwangen.de>
2023-03-22 23:24:33 +0100

    feat: benchmark testing for screenshots

--- image_analysis_test/take_screenshot.py
+++ image_analysis_test/take_screenshot.py
@@ -1,15 +1,36 @@
 from PIL import ImageGrab
+import mss.tools
 import win32gui
 import pygetwindow
+import time
 
-def screenshot(hwnd):
+def screenshotNew(hwnd, i):
   win32gui.SetForegroundWindow(hwnd)
-  bbox = win32gui.GetWindowRect(hwnd)
+  img = mss.mss().grab(bbox)
+  size = img.size 
+  path = r'assets\benchmarkNew\ss_{}.png'.format(i)
+  mss.tools.to_png(img.rgb, img.size, output=path)
+
+def screenshotOld(hwnd, i):
+  win32gui.SetForegroundWindow(hwnd)
   img = ImageGrab.grab(bbox)
   size = img.size 
-  img = img.crop((110, 35, 435, 530))
-  img.save(r'assets\ss.png')
+  img = img.crop((110, 35, 400, 530))
+  path = r'assets\benchmarkOld\ss_{}.png'.format(i)
+  img.save(path)
+
+def benchmark():
+  start = time.time()
+  i = 0
+  while i < 100:
+    screenshotOld(hwnd, i)
+    i += 1
+
+  end = time.time()
+  print (end - start)
+
 
 stepWindow = pygetwindow.getWindowsWithTitle('StepMania')
 hwnd = stepWindow[0]._hWnd
-screenshot(hwnd)
+bbox = win32gui.GetWindowRect(hwnd)
+benchmark()
